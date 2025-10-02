63d89629966186760d1db88aec2e1458efd37956
pkncoder <alex.h.preston@gmail.com>
2025-10-01 20:07:20 -0400

    New code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,31 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+
+  TalonFX motor = new TalonFX(31);
+  CommandXboxController controller = new CommandXboxController(0);
   
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
-    
+
+
+    controller.a().onTrue(
+      new InstantCommand(() -> {motor.set(1.0);})
+    ).onFalse(
+      new InstantCommand(() -> {motor.set(0.0);})
+    );
+
+
   }
 
   public Command getAutonomousCommand() {
